3b1f487cd633a9d3febe674614fdbde06f64fdd6
dkijania <dariusz@o1labs.org>
2025-03-05 17:17:15 +0100

    WIP

--- buildkite/scripts/debian/upload-to-gs.sh
+++ buildkite/scripts/debian/upload-to-gs.sh
@@ -3,7 +3,9 @@
 CODENAME=$1
 GS_DO_NOT_OVERRIDE=1
 
+. ./buildkite/scripts/storage/env.sh
+
 echo "--- Copy debians to gs"
-for entry in _build/*.deb; do
+for entry in ${STORAGE_FOLDER}/*.deb; do
   source ./buildkite/scripts/cache-artifact.sh $entry ${CODENAME}/$entry 
 done 
