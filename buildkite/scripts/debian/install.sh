de99e647161c60612dfcd473de01fefeb14b3076
dkijania <dariusz@o1labs.org>
2025-03-06 18:47:19 +0100

    fix root argument in calling cache/manager.sh

--- buildkite/scripts/debian/install.sh
+++ buildkite/scripts/debian/install.sh
@@ -40,7 +40,7 @@ else
       ;;
       mina-create-legacy-genesis)
         # Download locally static debians (for example mina-legacy-create-genesis )
-        source ./buildkite/scripts/cache/manager.sh read --root buildkite/shared/debs "$MINA_DEB_CODENAME/$i*" _build
+        source ./buildkite/scripts/cache/manager.sh read --root debs "$MINA_DEB_CODENAME/$i*" _build
       ;;
     esac
     source ./buildkite/scripts/cache/manager.sh debians/$MINA_DEB_CODENAME/${i}_* $LOCAL_DEB_FOLDER
