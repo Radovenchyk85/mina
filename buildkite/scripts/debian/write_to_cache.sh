4753d41a58fb784ce49fbef288db1d41e640169a
dkijania <dariusz@o1labs.org>
2025-03-06 20:50:36 +0100

    write to folder not file

--- buildkite/scripts/debian/write_to_cache.sh
+++ buildkite/scripts/debian/write_to_cache.sh
@@ -3,5 +3,5 @@
 CODENAME=$1
 
 for entry in _build/*.deb; do
-  ./buildkite/scripts/cache/manager.sh write "$entry" debians/${CODENAME}
+  ./buildkite/scripts/cache/manager.sh write "$entry" debians/${CODENAME}/
 done 
